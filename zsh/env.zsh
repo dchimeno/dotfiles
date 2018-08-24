52fcaadba35a806b8eecaaf9d90a08cb81cdfaa6
Daniel Chimeno <daniel@chimeno.me>
2018-08-24 10:17:08 +0200

    no brew analytics

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -6,6 +6,9 @@ export CLICOLOR=1
 export LSCOLORS=gxfxcxdxbxegedabagacad
 export LS_COLORS='di=36:fi=0:ln=35:pi=5:so=5:bd=5:cd=5:or=0;41:mi=0:ex=31'
 
+export HOMEBREW_NO_ANALYTICS=1
+
+
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
